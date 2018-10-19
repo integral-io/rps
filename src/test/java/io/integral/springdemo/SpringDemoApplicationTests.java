ffee75d71b6146524261f194c4612fb952ce36a0
xing <xing@integral.io>
2018-10-19 11:29:46 -0400

    Create first working test of a spring controller

--- src/test/java/io/integral/springdemo/SpringDemoApplicationTests.java
+++ src/test/java/io/integral/springdemo/SpringDemoApplicationTests.java
@@ -2,15 +2,44 @@ package io.integral.springdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringDemoApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void name() throws Exception {
+        // //How to write an API test in Spring Boot?
+        // We want to ensure our request is routed correctly
+            // if we send GET /shapes
+            // then return not 404
+        this.mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
 }
+/*
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+}
+ */
